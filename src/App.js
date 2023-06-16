3deda76172a20c53914a37976fe6b76e545c7277
Parth <parthpandya00@gmail.com>
2023-06-16 19:33:21 +0530

    all data saved

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+import { Typography, styled, Box } from '@mui/material';
+// components
+import Balance from './components/Balance';
+import ExpenseCard from './components/ExpenseCard';
+import NewTransaction from './components/NewTransaction';
+import Transactions from './components/Transactions';
+
+const Header = styled(Typography)`
+  // margin: 10px 0;
+  font-size: 36px;
+  color: #005fc4;
+  // text-transform: uppercase;
+`
+
+const Component = styled(Box)`
+display: flex;
+background: #f6f6f6;
+width: 800px;
+padding: 10px;
+border-radius: 10px;
+margin: auto;
+& > div {
+  // height: 72vh;
+  width: 50%;
+  padding: 10px;
+}
+`
 
 function App() {
+
+  const [transactions, setTransactions] = useState([
+   
+    { id: 1, text: 'salary', amount: 35000 },
+    { id: 2, text: 'dinner', amount: -200 }
+   
+  ]);   // in this array [] object saved {}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+        <Header>Expense Tracker</Header>
+        <Component>
+          <Box>
+            <Balance transactions={transactions}/>
+            <ExpenseCard transactions={transactions}></ExpenseCard>
+            <NewTransaction setTransactions={setTransactions}/>
+          </Box>
+          <Box style={{ flex: 1 }}>
+            <Transactions transactions={transactions} setTransactions={setTransactions}/>
+          </Box>
+        </Component>
+    </Box>
   );
 }
 
